5cf04c335ae985f69658bd8b648e78cce3584b39
CarlosH200 <carloshugop46@gmail.com>
2025-05-26 16:37:08 -0600

    desing funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { FormsModule } from '@angular/forms';
+import { EventCalendarComponent } from "./components/event-calendar/event-calendar.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     SideBarComponent,
-    LoginComponent,
+    FormsModule,
+    EventCalendarComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   hidePassword: boolean = true;
+  username: string = '';
+  password: string = '';
 }

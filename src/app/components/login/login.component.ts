db8b633505fed004435d401b72f906212773fe75
CarlosH200 <carloshugop46@gmail.com>
2025-11-15 08:33:59 -0600

    tema claro y obscuro implementado en pantalla de login funcionando

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SideBarComponent } from "../side-bar/side-bar.component";
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/authService';
 import { LoginModel } from '../../models/loginModel';
 import { ThemeService } from '../../services/theme.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, SideBarComponent, CommonModule],
+  imports: [
+    FormsModule,
+    SideBarComponent,
+    CommonModule,
+    MatSnackBarModule
+  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent {
   // Variale para validar si el usuario inicio sesión correctamente
@@ -24,32 +33,47 @@ export class LoginComponent {
   success: boolean | null = null;
 
   // inyeccion de los services
-  constructor(private authService: AuthService,  public theme: ThemeService) { }
+  constructor(
+    private authService: AuthService,  
+    public theme: ThemeService,
+    private snackBar: MatSnackBar
+  ) { }
 
   isLogin(): void {
-    // console.log('Intentando iniciar sesión con:', this.usuario, this.password);
-    this.authService.login(this.usuario, this.password).subscribe({
-      next: (res: LoginModel) => {
-        this.success = res.success;
-        this.mensaje = res.mensaje;
+  this.authService.login(this.usuario, this.password).subscribe({
+    next: (res: LoginModel) => {
+      this.success = res.success;
+      this.mensaje = res.mensaje;
 
-        if (res.success) {
-          // console.log('Login exitoso');
-          alert(this.mensaje);
-
-          this.isLoggingIn = true;
-          // Aquí puedes guardar token o redirigir
-        } else {
-          // console.warn('Credenciales inválidas');
-          alert(this.mensaje);
-          this.isLoggingIn = false;
-        }
-      },
-      error: (err) => {
-        this.success = false;
-        this.mensaje = 'Error al conectar con el servidor';
-        console.error(err);
+      if (res.success) {
+        this.snackBar.open(this.mensaje, 'Cerrar', {
+          duration: 3000,
+          panelClass: ['alerta-exito'], // opcional para estilos
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+        });
+        this.isLoggingIn = true;
+      } else {
+        this.snackBar.open(this.mensaje, 'Cerrar', {
+          duration: 3000,
+          panelClass: ['alerta-error'],
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+        });
+        this.isLoggingIn = false;
       }
-    });
-  }
+    },
+    error: (err) => {
+      this.success = false;
+      this.mensaje = 'Error al conectar con el servidor';
+      this.snackBar.open(this.mensaje, 'Cerrar', {
+        duration: 3000,
+        panelClass: ['alerta-error'],
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      });
+      console.error(err);
+    }
+  });
+}
 }
